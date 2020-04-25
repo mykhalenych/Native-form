9de1bda68a2cfd0c6c0f09c2a973308d368c3ee5
Mykhalenych <mykhalenych@gmail.com>
2020-04-25 21:50:45 +0300

    Updates

---
+++ precache-manifest.48ed3bb21ec86ebd023493929930fdc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36acc986ca57cc469842213b96c9b42",
+    "url": "/Native-form/index.html"
+  },
+  {
+    "revision": "c89c0193983edba7dd99",
+    "url": "/Native-form/static/css/main.6f9b54a5.chunk.css"
+  },
+  {
+    "revision": "00af306e0146d6c6c94a",
+    "url": "/Native-form/static/js/2.851e6245.chunk.js"
+  },
+  {
+    "revision": "885c0e205ef5e79f4aef448a818ba6a1",
+    "url": "/Native-form/static/js/2.851e6245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c89c0193983edba7dd99",
+    "url": "/Native-form/static/js/main.d2c06b76.chunk.js"
+  },
+  {
+    "revision": "523b589ea8e120b511c9",
+    "url": "/Native-form/static/js/runtime-main.e911bf84.js"
+  }
+]);
